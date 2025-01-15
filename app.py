0f80d4f4c9a0a0c1a7becbdb9f948955af4b56d0
climsc <limcheryl015@gmail.com>
2025-01-15 08:39:07 +0000

    1

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 from flask import Flask
 from flask import render_template,request
+
+app = Flask(__name__) 
+
+@app.route("/") #telling flask the first line to read is this
+def index(): #renders template
+    return(render_template("index.html"))
+
+if __name__ == "__main__": #confirming that the application belongs to us
+    app.run()
